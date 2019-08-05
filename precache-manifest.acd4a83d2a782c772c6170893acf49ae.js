48aaf4b2764b6b84b7fb6d9e115e3e6e2d10ff61
elisha-enu <elishaenu@gmail.com>
2019-08-05 21:42:10 +0700

    Updates

---
+++ precache-manifest.acd4a83d2a782c772c6170893acf49ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d114bf2753a41bbdd8f6c4e001a9c",
+    "url": "/pokemon-elishalimanu/index.html"
+  },
+  {
+    "revision": "9da2dc9ad83c6674f03b",
+    "url": "/pokemon-elishalimanu/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4da90dbd67ef2a817934",
+    "url": "/pokemon-elishalimanu/static/js/2.d6e6de33.chunk.js"
+  },
+  {
+    "revision": "9da2dc9ad83c6674f03b",
+    "url": "/pokemon-elishalimanu/static/js/main.c9662ee0.chunk.js"
+  },
+  {
+    "revision": "3fc9815b02627bc4bf07",
+    "url": "/pokemon-elishalimanu/static/js/runtime~main.378d24f6.js"
+  }
+]);
